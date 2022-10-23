0079788f4f6c6fca01389ab8fd07052367cba2a4
Alibek <effuone@github.com>
2022-10-23 19:17:12 +0600

    upcoming tasks added

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
-import puppeteer from "puppeteer";
-import cheerio from 'cheerio'
-import 'dotenv/config'
 import axios from "axios";
+import "dotenv/config";
+import cheerio from "cheerio";
+import * as ManageBacService from './services/ManageBacService'
+const originUrl = "https://isnur-sultan.managebac.com";
 
-const originUrl = 'https://isnur-sultan.managebac.com'
-
-async function main() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`${originUrl}/login`, { waitUntil: 'networkidle0' }); // wait until page load
-    await page.type('#session_login', process.env.MANAGEBAC_EMAIL);
-    await page.type('#session_password', process.env.MANAGEBAC_PASSWORD);
-    await Promise.all([
-        page.click('.btn-block'),
-        page.waitForNavigation({ waitUntil: 'networkidle0' }),
-    ]);
-    const cookies = await page.cookies()
-    let cookieString = ''
-    for (let i = 0; i < cookies.length; i++) {
-      const element = cookies[i];
-      cookieString += (element.name+element.value+'; ') 
-    }
-    const html = (await axios.get(`${originUrl}/student/tasks_and_deadlines`, {
-      withCredentials:true,
-      headers:{
-          "Cookie": cookieString
-      }
-    })).data
-    console.log(html)
-    // const $ = cheerio.load(html)
-    // $('.details', html).each(function(){
-    //   const linkElement = $(this).find('.title')
-    //   const taskName = linkElement.text()
-    //   const link = originUrl + linkElement.find('a').attr('href')
-    //   const subject = $(this).find('.group-name.a').attr('href')
-    //   console.log({taskName, link, subject})
-    // })
-    // console.log(textData)
-    // await page.screenshot({path: `./assets/images/managebac.png`})
-    // console.log('New Page URL:', page.url());
-    await browser.close();
+// TODO: add functionality for adding task details 
+async function getDetailsFromTask(taskUrl) {
+  const html = axios.get(taskUrl, {
+    withCredentials: true,
+    headers: {
+      Cookie: myCookie,
+    },
+  }).data;
+  const $ = cheerio.load(html);
+  const texts = [];
+  const detailsElememt = $(".fix-body-margins", html);
+  if (detailsElememt.length > 0) {
+    detailsElememt.find("p").each(function () {
+      texts.push($(this).text());
+    });
   }
-  
-await main();
-console.log('over')
+  return texts;
+}
--- services/ManageBacService.js
+++ services/ManageBacService.js
@@ -1,15 +1,92 @@
-import cheerio from 'cheerio'
-export default class ManageBacService {
-    async getCourses(page){
-        const html = await page.evaluate(() => document.querySelector('*').outerHTML);
-        const $ = cheerio.load(html)
-        const courses = []
-        $('.parent', html).find('li').each(function(){
-            const course = new Object();
-            course.name = ($(this).find('span').text());
-            course.link = originUrl + ($(this).find('a').attr('href'))
-            courses.push(course)
-        })
-        return courses
+import puppeteer from "puppeteer";
+import cheerio from "cheerio";
+import axios from 'axios'
+const originUrl = "https://isnur-sultan.managebac.com";
+  export const getCourses = (html) => {
+    const courses = [];
+    $(".parent", html)
+      .find("li")
+      .each(function () {
+        const course = new Object();
+        course.name = $(this).find("span").text();
+        course.link = originUrl + $(this).find("a").attr("href");
+        courses.push(course);
+      });
+    return courses;
+  }
+  export const getServiceCookies = async (email, password) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`${originUrl}/login`, { waitUntil: "networkidle0" }); // wait until page load
+    await page.type("#session_login", email);
+    await page.type("#session_password", password);
+    await Promise.all([
+      page.click(".btn-block"),
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+    ]);
+    const cookies = await page.cookies();
+    await browser.close();
+    // console.log(cookies)
+    const extractedCookies = [];
+    extractedCookies.push(
+      (({ name, value }) => ({ name, value }))(
+        cookies.find((item) => item.name === "__utma")
+      )
+    );
+    extractedCookies.push(
+      (({ name, value }) => ({ name, value }))(
+        cookies.find((item) => item.name === "__utmc")
+      )
+    );
+    extractedCookies.push(
+      (({ name, value }) => ({ name, value }))(
+        cookies.find((item) => item.name === "__utmz")
+      )
+    );
+    extractedCookies.push(
+      (({ name, value }) => ({ name, value }))(
+        cookies.find((item) => item.name === "user_id")
+      )
+    );
+    extractedCookies.push(
+      (({ name, value }) => ({ name, value }))(
+        cookies.find((item) => item.name === "user")
+      )
+    );
+    extractedCookies.push(
+      (({ name, value }) => ({ name, value }))(
+        cookies.find((item) => item.name === "_managebac_session")
+      )
+    );
+    let cookieString = "";
+    for (let i = 0; i < extractedCookies.length; i++) {
+      const element = extractedCookies[i];
+      cookieString += element.name + "=" + element.value + "; ";
     }
+    return cookieString;
+  }
+  export const getUpcomingTasks = async (cookie) => {
+    const html = (await axios.get(originUrl + '/student/tasks_and_deadlines', {
+      withCredentials: true,
+      headers: {
+        "Cookie": cookie
+      }
+    })).data
+    const $ = cheerio.load(html)
+    
+    const tasks = []
+    $('.details', html).each(function(){
+      const task = new Object();
+      const taskElement = $(this).find('.title')
+      task.subject = $(this).find('.group-name').text().trim()
+      task.title = taskElement.text().trim()
+      task.taskType = ''
+      $(this).find('.label').each(function(){
+        task.taskType += ($(this).text().trim() + ' ')
+      })
+      task.dueDate = $(this).find('.due').text().trim()
+      task.url = originUrl + taskElement.find('a').attr('href')
+      tasks.push(task)
+    })
+    return tasks
 }
